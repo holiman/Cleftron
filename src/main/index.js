4ee9b6d0962f311bb4c27ca008b43300b4f8ffe7
Ryan Ghods <ralxzryan@gmail.com>
2018-05-28 13:10:20 -0700

    Add eslint and prettier (#10)

--- src/main/index.js
+++ src/main/index.js
@@ -1,183 +1,234 @@
-import { Notification, Menu, BrowserWindow, Tray, app, nativeImage, ipcMain, dialog } from 'electron'
+import {
+  Notification,
+  Menu,
+  BrowserWindow,
+  Tray,
+  app,
+  nativeImage,
+  ipcMain,
+  dialog
+} from 'electron';
 
-import fs from 'fs'
-import path from 'path'
-import StringDecoder from 'string_decoder'
-import jsonrpc from 'jsonrpc-lite'
-import { spawn } from 'cross-spawn'
+import fs from 'fs';
+import path from 'path';
+import StringDecoder from 'string_decoder';
+import jsonrpc from 'jsonrpc-lite';
+import { spawn } from 'cross-spawn';
+
 const decoder = new StringDecoder.StringDecoder('utf8');
 
 if (process.env.NODE_ENV !== 'development') {
-	global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+  global.__static = require('path')
+    .join(__dirname, '/static')
+    .replace(/\\/g, '\\\\');
 }
 
-let mainWindow
-let clef
+let mainWindow;
+let clef;
 
-const winURL = process.env.NODE_ENV === 'development'
-	? `http://localhost:9080`
-	: `file://${__dirname}/index.html`
+const winURL =
+  process.env.NODE_ENV === 'development'
+    ? `http://localhost:9080`
+    : `file://${__dirname}/index.html`;
 
-// Set up the systray icon 
-function setupTray(){
+// Set up the systray icon
+function setupTray() {
+  const iconPath = path.join(__static, 'blu-eth-48x48.png');
+  let tray = new Tray(nativeImage.createFromPath(iconPath));
 
-	const iconPath = path.join(__static, 'blu-eth-48x48.png');
-	let tray = new Tray(nativeImage.createFromPath(iconPath));
-
-	const menu = Menu.buildFromTemplate([
-		{label: "Open", click: (item, window, event) => {
-				mainWindow.show();
-		}},
-		{type: "separator"},
-		{label: "Exit", click : () => {
-			app.isQuiting = true;
-			app.quit();
-		}},
-	]);
-	tray.setContextMenu(menu);
-	tray.on('click', () => {
-		mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
-	})
-
+  const menu = Menu.buildFromTemplate([
+    {
+      label: 'Open',
+      click: (item, window, event) => {
+        mainWindow.show();
+      }
+    },
+    { type: 'separator' },
+    {
+      label: 'Exit',
+      click: () => {
+        app.isQuiting = true;
+        app.quit();
+      }
+    }
+  ]);
+  tray.setContextMenu(menu);
+  tray.on('click', () => {
+    mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
+  });
 }
 
 // Display a notification to the user
-function note(text, surpressOnClick){
-	let notify = new Notification({ title: 'Cleftron', body: text})
-	notify.show();
-	if (surpressOnClick){ return }
-	notify.on('click', () =>{ mainWindow.show(); })
+function note(text, surpressOnClick) {
+  let notify = new Notification({ title: 'Cleftron', body: text });
+  notify.show();
+  if (surpressOnClick) {
+    return;
+  }
+  notify.on('click', () => {
+    mainWindow.show();
+  });
 }
 
-function createWindow () {
-	mainWindow = new BrowserWindow({
-		height: 1000,
-		useContentSize: true,
-		width: 563
-	})
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    height: 1000,
+    useContentSize: true,
+    width: 563
+  });
 
-	mainWindow.loadURL(winURL)
-	//mainWindow.webContents.openDevTools()
-	mainWindow.on('closed', () => { mainWindow = null })
-	mainWindow.on('show', () => {})
-	mainWindow.on('hide', () => {})
-	setupTray();
+  mainWindow.loadURL(winURL);
+  //mainWindow.webContents.openDevTools()
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+  mainWindow.on('show', () => {});
+  mainWindow.on('hide', () => {});
+  setupTray();
 
+  mainWindow.on('close', e => {
+    if (!app.isQuiting) {
+      e.preventDefault();
+      e.sender.hide();
+    }
+  });
+  var send = function(data) {
+    console.log('->' + data);
+    clef.stdin.write(data);
+    clef.stdin.write('\n');
+  };
+  ipcMain.on('response', (e, m) => {
+    mainWindow.webContents.send('message', m);
+    send(m);
+  });
+  // From the landing page, we send 'channelsConfigured' when
+  // the receivers are configured, and ready to receive data
+  ipcMain.once('channelsConfigured', () => {
+    clef.stderr.on('data', data => {
+      mainWindow.webContents.send('stderr', data);
+      console.log(String(data));
+    });
+  });
+  let handlers = {
+    ApproveTx: {
+      msg: () => 'Transaction Signing is awaiting review.',
+      ui: true
+    },
+    ApproveExport: {
+      msg: () => 'Account Export is awaiting review.',
+      ui: true
+    },
+    ApproveImport: {
+      msg: () => 'Account Import is awaiting review.',
+      ui: true
+    },
+    ApproveSignData: {
+      msg: () => 'Message signing is awaiting review.',
+      ui: true
+    },
+    ApproveListing: {
+      msg: () => 'Account listing is awaiting review.',
+      ui: true
+    },
+    ApproveNewAccount: {
+      msg: () => 'New account request is awaiting review.',
+      ui: true
+    },
+    // These ones do not have a separate UI
+    ShowInfo: { msg: data => data.params[0].text },
+    ShowError: { msg: data => data.params[0].text },
+    OnApprovedTx: { msg: data => 'Signed ' + data.params[0].tx.hash },
+    OnSignerStartup: {
+      msg: data =>
+        'Clef is up. Web: ' +
+        data.params[0].info.extapi_http +
+        ' IPC:' +
+        data.params[0].info.extapi_ipc
+    }
+  };
+  clef.stdout.on('data', data => {
+    console.log(data.toString());
+    let rpc = jsonrpc.parse(decoder.end(data));
+    if (rpc.type != 'request') {
+      console.log('wtf? ' + rpc);
+      return;
+    }
+    let payload = rpc.payload;
+    let handler = handlers[payload.method];
+    if (handler) {
+      note(handler.msg(payload));
+      if (handler.ui) {
+        mainWindow.webContents.send('ApprovalRequired', payload);
+      } else {
+        // ShowInfo, ShowError, OnSignerStartup, OnApprovedTx
+        send(JSON.stringify(jsonrpc.success(payload.id, {})));
+      }
+      return;
+    }
+    console.log('Missing handler for method ' + payload.method);
+    return;
+  });
 
-	mainWindow.on('close', (e) => {
-		if(!app.isQuiting){
-		e.preventDefault();
-		e.sender.hide();
-		}
-	})
-	var send = function(data){
-		console.log("->"+data)
-		clef.stdin.write(data);
-		clef.stdin.write("\n");
-	}
-	ipcMain.on('response', (e, m) => {
-		mainWindow.webContents.send("message", m);
-		send(m);
-	})
-	// From the landing page, we send 'channelsConfigured' when
-	// the receivers are configured, and ready to receive data
-	ipcMain.once('channelsConfigured', () =>{
-		clef.stderr.on('data', (data) => {
-			mainWindow.webContents.send('stderr', data);
-			console.log(String(data));
-		});
-	})
-	let handlers = {
-		ApproveTx :          { msg: () => 'Transaction Signing is awaiting review.', ui:true},
-		ApproveExport:       { msg: () => 'Account Export is awaiting review.', ui:true},	
-		ApproveImport:       { msg: () => 'Account Import is awaiting review.', ui:true},
-		ApproveSignData:     { msg: () => 'Message signing is awaiting review.', ui:true},
-		ApproveListing:      { msg: () => 'Account listing is awaiting review.', ui:true},
-		ApproveNewAccount:   { msg: () => 'New account request is awaiting review.', ui:true},
-		// These ones do not have a separate UI
-		ShowInfo:            { msg: (data) => data.params[0].text},
-		ShowError:           { msg: (data) => data.params[0].text},
-		OnApprovedTx:        { msg: (data) => ('Signed '+data.params[0].tx.hash)},
-		OnSignerStartup:     { msg: (data) => ('Clef is up. Web: '+data.params[0].info.extapi_http +" IPC:" + data.params[0].info.extapi_ipc)},
-	}
-	clef.stdout.on('data', (data) => {
-		console.log(data.toString())
-		let rpc = jsonrpc.parse(decoder.end(data))
-		if (rpc.type != "request"){ console.log("wtf? "+ rpc); return;}
-		let payload = rpc.payload
-		let handler = handlers[payload.method]  
-		if(handler) { 
-			note(handler.msg(payload))
-			if (handler.ui){
-				mainWindow.webContents.send('ApprovalRequired', payload)
-			}else{
-				// ShowInfo, ShowError, OnSignerStartup, OnApprovedTx
-				send(JSON.stringify(jsonrpc.success(payload.id,{})));
-			}
-			return 
-		}
-		console.log("Missing handler for method "+payload.method);
-		return
-	});
-	
-	clef.on('exit', (code) => {
-		throw(code)
-		mainWindow.webContents.send('message', `Child exited with code ${code}`);
-	});
-
-
+  clef.on('exit', code => {
+    throw code;
+    mainWindow.webContents.send('message', `Child exited with code ${code}`);
+  });
 }
 
-app.on('ready', function () {
+app.on('ready', function() {
+  let clefpath = '/home/user/tools/clef/clef';
 
-	let clefpath = "/home/user/tools/clef/clef"
+  if (process.argv[1] === '--clefbin') {
+    clefpath = process.argv[2];
+  }
 
-	if(process.argv[1] === '--clefbin'){
-		clefpath = process.argv[2];
-	} 
+  if (!clefpath) {
+    clefpath = dialog.showOpenDialog({
+      title: 'Select Clef Binary',
+      filters: [
+        { name: 'Application', extensions: ['exe', 'bin', 'app'] },
+        { name: 'All Files', extensions: ['*'] }
+      ]
+    })[0];
+  }
 
-	if (!clefpath) {
-		clefpath = dialog.showOpenDialog({
-			title : 'Select Clef Binary',
-			filters : [
-				{name: 'Application', extensions: ['exe', 'bin', 'app']},
-				{name: 'All Files', extensions: ['*']}
-			]
-		})[0];
-	}
+  try {
+    clef = spawn(clefpath, [
+      '--rpc',
+      '--4bytedb',
+      path.dirname(clefpath) + '/4byte.json',
+      '--ipcdisable',
+      '--stdio-ui'
+    ]);
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
 
-	try{
-		clef = spawn(clefpath, ['--rpc','--4bytedb' ,path.dirname(clefpath) + '/4byte.json','--ipcdisable','--stdio-ui'])
-	} catch (e) {
-		console.log(e);
-		throw(e)
-	}
-
-	createWindow()
-	let fd = require('fs').createReadStream(clefpath);
-	let hash = require('crypto').createHash('sha256');
-	hash.setEncoding('hex');
-	fd.on('end', function() {
-			hash.end();
-			const notify = new Notification({
-				title: 'Binary Sha256',
-				body: hash.read(),
-				silent: true
-			})
-			notify.show();
-	});
-	fd.pipe(hash);
-
-})
+  createWindow();
+  let fd = require('fs').createReadStream(clefpath);
+  let hash = require('crypto').createHash('sha256');
+  hash.setEncoding('hex');
+  fd.on('end', function() {
+    hash.end();
+    const notify = new Notification({
+      title: 'Binary Sha256',
+      body: hash.read(),
+      silent: true
+    });
+    notify.show();
+  });
+  fd.pipe(hash);
+});
 
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit()
-	}
-})
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
 
 app.on('activate', () => {
-	if (mainWindow === null) {
-		createWindow()
-	}
-})
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,93 +1,92 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-
-Vue.use(Vuex)
+Vue.use(Vuex);
 export default new Vuex.Store({
-	state: {
-		// A text description about what UI to use
-		ui: null,
-		// Log messages, from the clef binary
-		logMessages: [],
-		// Requests to be handled
-		tasks: {}, 
-		// The currently selected task
-		selected: {},
+  state: {
+    // A text description about what UI to use
+    ui: null,
+    // Log messages, from the clef binary
+    logMessages: [],
+    // Requests to be handled
+    tasks: {},
+    // The currently selected task
+    selected: {}
+  },
+  mutations: {
+    addTask(state, task) {
+      // state.tasks[task.id] = task
+      if (!state.selected.id) {
+        state.selected = task;
+        state.ui = state.selected.ui;
+      }
 
-	},
-	mutations: {
-		addTask(state, task){
-			// state.tasks[task.id] = task
-			if (!state.selected.id){
-				state.selected = task
-				state.ui = state.selected.ui
-			}
+      // Since state.tasks in an object,we need to use this
+      // construct intead of
+      // state.tasks[task.id] = task
+      // See https://vuejsdevelopers.com/2017/03/05/vue-js-reactivity/
 
-			// Since state.tasks in an object,we need to use this 
-			// construct intead of 
-			// state.tasks[task.id] = task
-			// See https://vuejsdevelopers.com/2017/03/05/vue-js-reactivity/
+      Vue.set(state.tasks, task.id, task);
+    },
+    // Signal that a task is done
+    taskDone(state) {
+      if (!state.selected.id) {
+        return;
+      }
+      delete state.tasks[state.selected.id];
+      state.selected = {};
+      state.ui = null;
+      // Go to next task, sort by rpc id (key)
+      let keys = Object.keys(state.tasks).sort();
+      if (!keys.length) {
+        return;
+      }
+      state.selected = state.tasks[keys[0]];
+      state.ui = state.selected.ui;
+    },
 
-			Vue.set(state.tasks, task.id, task);
-		},
-		// Signal that a task is done
-		taskDone(state){
-			if (!state.selected.id){
-				return;
-			}
-			delete(state.tasks[state.selected.id])
-			state.selected = {};
-			state.ui = null;
-			// Go to next task, sort by rpc id (key)
-			let keys = Object.keys(state.tasks).sort();
-			if (!keys.length) {return;}
-			state.selected = state.tasks[keys[0]]
-			state.ui = state.selected.ui
-		},
-
-		addLog(state,data){
-			state.logMessages.push(data)
-		},
-		setSelectedTask(state, data){
-			var id = Number(data)
-			if (state.tasks[id]){
-				state.selected = state.tasks[id]
-				state.ui = state.selected.ui
-			}	
-		},
-		updateObject(state, data){
-			state.selected.obj = data;
-		}
-	},
-	actions: {
-		selectTask({commit}, data){
-			// Select by RPC ID
-			commit('setSelectedTask', Number(data))
-		},
-		taskDone({state, commit}, data){
-			commit('taskDone');
-		},
-		addData({state, commit}, data){
-			commit('addTask', {
-				strData: JSON.stringify(data),
-				ui  : data.method,
-				obj : data,
-				id : data.id
-			})
-		},
-		// Update an object with new data, e.g. setting gasPrice
-		updateObject({state, commit}, data){
-			commit('updateObject', data)
-		},
-		// addPassphrase({commit}, data){
-		// 	commit('SAVE_PASS', data)
-		// },
-		logMsg({commit}, data){
-			commit('addLog', data)
-		},
-	}
-})
-
+    addLog(state, data) {
+      state.logMessages.push(data);
+    },
+    setSelectedTask(state, data) {
+      var id = Number(data);
+      if (state.tasks[id]) {
+        state.selected = state.tasks[id];
+        state.ui = state.selected.ui;
+      }
+    },
+    updateObject(state, data) {
+      state.selected.obj = data;
+    }
+  },
+  actions: {
+    selectTask({ commit }, data) {
+      // Select by RPC ID
+      commit('setSelectedTask', Number(data));
+    },
+    taskDone({ state, commit }, data) {
+      commit('taskDone');
+    },
+    addData({ state, commit }, data) {
+      commit('addTask', {
+        strData: JSON.stringify(data),
+        ui: data.method,
+        obj: data,
+        id: data.id
+      });
+    },
+    // Update an object with new data, e.g. setting gasPrice
+    updateObject({ state, commit }, data) {
+      commit('updateObject', data);
+    },
+    // addPassphrase({commit}, data){
+    // 	commit('SAVE_PASS', data)
+    // },
+    logMsg({ commit }, data) {
+      commit('addLog', data);
+    }
+  }
+});
 
 /**
 { "jsonrpc":"2.0",
